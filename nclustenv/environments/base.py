ff32fa57c2954508c70ee14fc050c02599f527e6
pedro <pgcotovio@gmail.com>
2021-06-22 21:58:23 +0100

    [implement][refactor][docs] base env
    -finish step implementation + docs
    -refactor into abstract base class
    -minor changes for compability

--- nclustenv/environments/base.py
+++ nclustenv/environments/base.py
@@ -1,16 +1,18 @@
+from abc import ABC
 from statistics import mean
 import numpy as np
 
 import gym
-from gym import spaces
+from gym import spaces, logger
 from gym.utils import seeding
 
-from nclustenv.states.base import BaseState
-from nclustenv.utils.actions import Action
-from nclustenv.utils import metrics
+from ..utils.actions import Action
+from ..utils import metrics
+
+# TODO implement render(bic/tric), parse params, reset
 
 
-class NClusterEnv(gym.Env):
+class BaseEnv(gym.Env, ABC):
 
     """
     Bi/Tricluster environment
@@ -18,24 +20,10 @@ class NClusterEnv(gym.Env):
 
     metadata = {'render.modes': ['human']}
 
-    # TODO implement init
     def __init__(self, smax, smin, clusters, noise, missings, seed=None,
                  metric='match_score_1_n', max_steps=200, error_margin=0.05, penalty=0.001):
 
-        super(NClusterEnv, self).__init__()
-
-        # Init
-
-        self.np_random = None
-        self.state = None
-        self.last_distances = None
-        self.current_step = None
-
-        self.seed(seed)
-
-        self.action_space = spaces.Tuple((spaces.Discrete(2),
-                                          spaces.Box(low=0.0, high=1.0, shape=[1, ], dtype=np.float16)))
-        self.observation_space = spaces.Box(low=np.array(smin), high=np.array(smax), dtype=np.int32)
+        super(BaseEnv, self).__init__()
 
         # Environment attributes
 
@@ -48,38 +36,90 @@ class NClusterEnv(gym.Env):
         self.target = 1.0 - error_margin
         self.penalty = penalty
 
+        self.action_space = spaces.Tuple((spaces.Discrete(2),
+                                          spaces.Box(low=0.0, high=1.0, shape=[1, ], dtype=np.float16)))
+        self.observation_space = spaces.Box(low=np.array(smin), high=np.array(smax), dtype=np.int32)
+
+        # Init
+
+        self.np_random = None
+        self.state = None
+        self.last_distances = None
+        self.current_step = None
+        self.steps_beyond_done = None
+        self.done = None
+
+        self.seed(seed)
+
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def step(self, action):
 
-        self.current_step += 1
-        action_ = Action(*action)
+        """
+        Run one timestep of the environment's dynamics. When end of episode is reached, you are responsible for calling
+        `reset()` to reset this environment's state. Accepts an action and returns a tuple (observation, reward, done,
+        info).
 
-        # Take action
-        getattr(self.state, action_.action)(action_.parameter)
+        Parameters
+        ----------
 
-        # calculate
-        self.last_distances.pop(0)
-        self.last_distances.append(self.volume_match)
+        action: list
+            An action provided by the agent.
 
-        # check state
+        Returns
+        -------
 
-        if self.last_distances[-1] == 1.0:
-            reward = self.get_reward(self.last_distances, True)
-            done = True
-        elif mean(self.last_distances) >= self.target:
-            reward = self.get_reward(self.last_distances, True, True)
-            done = True
-        elif self.current_step > self.max_step:
-            reward = -1
-            done = True
+            object
+                Agent's observation of the current environment.
+            float
+                Amount of reward returned after previous action.
+            bool
+                Whether the episode has ended, in which case further step() calls will return undefined results.
+            dict
+                Contains auxiliary diagnostic information (helpful for debugging, and sometimes learning).
+
+        """
+
+        if not self.done:
+            self.current_step += 1
+            action_ = Action(*action)
+
+            # Take action
+            getattr(self.state, action_.action)(action_.parameter)
+
+            # calculate
+            self.last_distances.pop(0)
+            self.last_distances.append(self.volume_match)
+
+            # check state
+
+            if self.last_distances[-1] == 1.0:
+                reward = self.get_reward(self.last_distances, True)
+                self.done = True
+            elif mean(self.last_distances) >= self.target:
+                reward = self.get_reward(self.last_distances, True, True)
+                self.done = True
+            elif self.current_step > self.max_steps:
+                reward = -1.0
+                self.done = True
+            else:
+                reward = self.get_reward(self.last_distances)
+
         else:
-            reward = self.get_reward(self.last_distances)
-            done = False
+            if self.steps_beyond_done == 0:
+                logger.warn(
+                    "You are calling 'step()' even though this "
+                    "environment has already returned done = True. You "
+                    "should always call 'reset()' once you receive 'done = "
+                    "True' -- any further steps are undefined behavior."
+                )
 
-        return self.state.state, reward, done, {}
+            self.steps_beyond_done += 1
+            reward = 0.0
+
+        return self.state.current, reward, self.done, {}
 
     def get_reward(self, last_distances, goal=False, error=False):
 
@@ -110,15 +150,15 @@ class NClusterEnv(gym.Env):
 
         """
 
-        return float(self.metric(self.state.shape, self.state.cluster_index, self.state.hclusters))
+        return float(self.metric(self.state.shape, self.state.cluster, self.state.hclusters))
 
-    # TODO implement reset
     def reset(self):
 
         self.current_step = 0
+        self.steps_beyond_done = 0
         self.last_distances = [0.0, 0.0, 0.0]
+        self.done = False
 
-    # TODO implement render
-    def render(self, mode='human'):
+    def _render(self):
 
         pass
