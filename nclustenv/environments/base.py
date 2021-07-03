4d8e2921e1dae472d5d76282dde53b6d14488c4a
pedro <pgcotovio@gmail.com>
2021-07-03 22:34:40 +0100

    [docstrings] base env

--- nclustenv/environments/base.py
+++ nclustenv/environments/base.py
@@ -14,7 +14,9 @@ from ..utils.helper import loader
 class BaseEnv(gym.Env, ABC):
 
     """
-    Bi/Tricluster environment
+    Abstract class from where dimensional specific subclasses should inherit. Should not be called directly.
+    This class abstracts dimensionality providing core implemented methods and abstract methods that should be
+    implemented for any n-clustering environment.
     """
 
     metadata = {'render.modes': ['human']}
@@ -31,6 +33,67 @@ class BaseEnv(gym.Env, ABC):
             error_margin=0.05,
             penalty=0.001
     ):
+
+        """
+        Parameters
+        ----------
+
+        shape: list, default [[100, 100, 2], [200, 200, 5]]
+            List of length 2 where the first element is the minimum shape the observation space and the second is the
+            maximum.
+        clusters: [int], default [1, 1]
+            List of length 2 where the first element is the minimum number of cluster to be hidden in the environment
+            and the second is the maximum.
+        dataset_settings: dict, default {}
+            Dataset settings to be passed to generator.
+
+            Note
+            ----
+                Parameters `silence` and `in_memory` should not be set, and will be overwritten.
+
+        seed: int, default None
+            Seed to initialize random object.
+        metric: str or class, default 'match_score_1_n'.
+            The name of a metric implemented in `utils.metrics`, or a pointer for a personalised metric.
+
+            ================ ===========
+            Implemented metrics
+            ----------------------------
+            name             task
+            ================ ===========
+            match_score_1_n  single
+            ================ ===========
+
+        action: str or class, default 'Action'.
+            The name of a action class implemented in `utils.actions`, or a pointer for a personalised action class.
+        max_steps: int, default 200
+            Maximum number of actions an agent can perform in a given environment.
+        error_margin: float, default 0.05
+            Margin of error for agent.
+        penalty: float, default 0.001
+            Penalty on reward per timestep (discount factor).
+
+        Attributes
+        ----------
+
+        dataset_settings: dict
+            Dataset settings to be passed to generator.
+        max_steps: int
+            Maximum number of actions an agent can perform in a given environment.
+        target: float
+            Target margin for agent.
+        penalty: float
+            Penalty on reward per timestep (discount factor).
+        action_space: gym space
+            Space from where the agent samples actions.
+        observation_space: gym space
+            Space from where the environment samples observations.
+        np_random: numpy random object
+            Random object.
+        state: state object
+            State object.
+
+        """
 
         super(BaseEnv, self).__init__()
 
@@ -75,6 +138,27 @@ class BaseEnv(gym.Env, ABC):
         self.seed(seed)
 
     def seed(self, seed=None):
+
+        """
+        Sets the seed for this env's random number generator(s).
+
+        Note
+        ----
+            Some environments use multiple pseudorandom number generators.
+            We want to capture all such seeds used in order to ensure that
+            there aren't accidental correlations between multiple generators.
+
+        Returns
+        -------
+
+            [int]
+                Returns the list of seeds used in this env's random
+                number generators. The first value in the list should be the
+                "main" seed, or the value which a reproducer should pass to
+                'seed'. Often, the main seed equals the provided 'seed', but
+                this won't be true if seed=None, for example.
+        """
+
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
@@ -177,10 +261,36 @@ class BaseEnv(gym.Env, ABC):
 
     @property
     def best_match(self):
+
+        """
+        Returns index of the hidden cluster with the best match for the current found cluster.
+
+        Returns
+        -------
+
+            int
+                Current best match.
+
+        """
+
         matches = self._metric(self.state.cluster, self.state.hclusters)
         return matches.index(min(matches))
 
     def reset(self):
+
+        """
+        Resets the environment to an initial state and returns an initial observation.
+        Note that this function should not reset the environment's random
+        number generator(s); random variables in the environment's state should
+        be sampled independently between multiple calls to `reset()`. In other
+        words, each call of `reset()` should yield an environment suitable for
+        a new episode, independent of previous episodes.
+
+        Returns
+        -------
+            observation (object)
+                The initial observation.
+        """
 
         # reset loggers
         self._current_step = 0
@@ -207,9 +317,46 @@ class BaseEnv(gym.Env, ABC):
 
     @abc.abstractmethod
     def _render(self, index):
+
+        """
+        Renders a given cluster.
+
+        Parameters
+        ----------
+
+        index: [[int]]
+            indexes of cluster on current environment.
+        """
         pass
 
     def render(self, mode='human'):
+
+        """
+        Renders the environment.
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.) By convention,
+        if mode is:
+        - human: render to the current display or terminal and
+          return nothing. Usually for human consumption.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+          representing RGB values for an x-by-y pixel image, suitable
+          for turning into a video.
+        - ansi: Return a string (str) or StringIO.StringIO containing a
+          terminal-style text representation. The text can include newlines
+          and ANSI escape sequences (e.g. for colors).
+
+        Note
+        ----
+            Make sure that your class's metadata 'render.modes' key includes
+            the list of supported modes. It's recommended to call super()
+            in implementations to use the functionality of this method.
+
+        Parameters
+        ----------
+
+        mode: str, default 'human'
+            The mode to render with.
+        """
 
         prefix = ''
         if not self._done:
