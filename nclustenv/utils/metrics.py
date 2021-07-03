6d2479659b419b34444b60cc2f9c40ba217e6387
pedro <pgcotovio@gmail.com>
2021-07-03 22:36:09 +0100

    [docstrings] metrics

--- nclustenv/utils/metrics.py
+++ nclustenv/utils/metrics.py
@@ -1,10 +1,19 @@
 
 def IoU(x, y):
 
+    """
+    Returns the intersection over union for cluster `x`,  and cluster `y`.
+    """
+
     intersection = sum([len(list(set(sx).intersection(sy))) for sx, sy in zip(x, y)])
     union = sum([(len(sx) + len(sy)) for sx, sy in zip(x, y)]) - intersection
     return float(intersection) / union
 
 
-def match_score_1_n(fbics, hbicsq):
-    return [IoU(fbics, y) for y in hbicsq]
+def match_score_1_n(fclust, hclusts):
+
+    """
+    For a given cluster `fclust` (found cluster), returns an IoU comparison against every element of `hclusts`
+    (hidden clusters).
+    """
+    return [IoU(fclust, y) for y in hclusts]
