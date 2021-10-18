2d117376c57da05dbc8b581cdca6e36a0d1d75a6
pedro <pgcotovio@gmail.com>
2021-10-18 16:56:55 +0100

    [implement] offline envs

--- nclustenv/environments/classic_lr/triclusterenv.py
+++ nclustenv/environments/classic_lr/triclusterenv.py
@@ -1,6 +1,6 @@
 
 from .base import BaseEnv
-from nclustenv.utils.states import State
+from nclustenv.utils.states import State, OfflineState
 from nclustenv.utils.helper import tensor_to_string, index_to_tensor
 
 from gym import spaces
@@ -24,7 +24,8 @@ class TriclusterEnv(BaseEnv):
             action='Action',
             max_steps=200,
             error_margin=0.05,
-            penalty=0.001
+            penalty=0.001,
+            init_state=True
     ):
 
         if shape is None:
@@ -54,8 +55,67 @@ class TriclusterEnv(BaseEnv):
             penalty=penalty
         )
 
-        self.state = State(generator='TriclusterGenerator', n=n, np_random=self.np_random)
-        self.reset()
+        if init_state:
+            self.state = State(generator='TriclusterGenerator', n=n, np_random=self.np_random)
+            self.reset()
 
     def _render(self, index):
         print(tensor_to_string(index_to_tensor(self.state.as_dense, index), index))
+
+
+class OfflineTriclusterEnv(TriclusterEnv):
+    """
+    This class provides an implementation of an offline two-dimensional gym environment with hidden biclusters.
+    """
+
+    def __init__(
+            self,
+            dataset,
+            n=None,
+            seed=None,
+            metric='match_score',
+            action='Action',
+            max_steps=200,
+            error_margin=0.05,
+            penalty=0.001,
+            train_test_split=0.8
+    ):
+        super(OfflineTriclusterEnv, self).__init__(
+            shape=None,
+            n=None,
+            clusters=None,
+            dataset_settings=None,
+            seed=seed,
+            metric=metric,
+            action=action,
+            max_steps=max_steps,
+            error_margin=error_margin,
+            penalty=penalty,
+            init_state=False
+        )
+
+        self.state = OfflineState(dataset=dataset, train_test_split=train_test_split, n=n, np_random=self.np_random)
+        self.reset()
+
+    def reset(self, train=True):
+        """
+        Resets the environment to an initial state and returns an initial observation.
+        Note that this function should not reset the environment's random
+        number generator(s); random variables in the environment's state should
+        be sampled independently between multiple calls to `reset()`. In other
+        words, each call of `reset()` should yield an environment suitable for
+        a new episode, independent of previous episodes.
+
+        Returns
+        -------
+            observation (object)
+                The initial observation.
+        """
+
+        # reset loggers
+        self._current_step = 0
+        self._steps_beyond_done = 0
+        self._last_distances = [1.0, 1.0, 1.0]
+        self._done = False
+
+        return self.state.reset(train=train)
