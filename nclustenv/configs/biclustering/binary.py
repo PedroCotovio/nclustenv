92a6e5e00fe86820d019052cd951bacbe2e2e7eb
pedro <pgcotovio@gmail.com>
2022-02-28 18:23:37 +0000

    [add] basic configs

--- nclustenv/configs/biclustering/binary.py
+++ nclustenv/configs/biclustering/binary.py
@@ -1,4 +1,20 @@
 
+basic = {
+            'shape': [[10, 10], [10, 10]],
+            'n': 1,
+            'clusters': [1, 1],
+            'dataset_settings': {
+                'dstype': dict(value='Symbolic'),
+                'patterns': dict(value=[['CONSTANT', 'CONSTANT']]),
+                'symbols': dict(value=[-1, 1]),
+                'bktype': dict(value='UNIFORM'),
+                'clusterdistribution': dict(value=[['UNIFORM', 4, 6], ['UNIFORM', 2, 4]]),
+                'contiguity': dict(value=None),
+                'plaidcoherency': dict(value='NO_OVERLAPPING')
+            },
+            'max_steps': 1000,
+}
+
 base = {
             'shape': [[100, 10], [100, 10]],
             'n': 5,
